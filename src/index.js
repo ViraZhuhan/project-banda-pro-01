4304f7025cab4ff0998f1def71d42124046b4258
Vita Nychvoloda <vitanychvoloda@yahoo.com>
2023-05-04 22:38:47 +0300

    Updated css for markup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import { Api } from './js/api';
-const a = new Api();
-a.dayTrends()
-  .then(res => console.log(res))
-  .catch(er => console.log(er.message));
+import createUpcominfOfNewMovieCard from './templates/upcoming.hbs'
+// const a = new Api();
+// a.dayTrends()
+//   .then(res => console.log(res))
+//   .catch(er => console.log(er.message));
+
+const newMovies = new Api();
+const refs = {
+  galleryOfNewMovies: document.querySelector('.upcoming'),
+  remindBtn: document.querySelector('.upcoming__button')
+  
+}
+newMovies.upcoming()
+.then(res => {
+  console.log(res.results[0]);
+  const markup = createUpcominfOfNewMovieCard(res.results[0]);
+  console.log(markup);
+  refs.galleryOfNewMovies.insertAdjacentHTML('beforeend', markup);
+})
+.catch(er =>console.log(er.message));
+
+
+
+function renderUpcomingMovie(res){
+
+try{
+  const markupUpcomingMovie = createUpcominfOfNewMovieCard(res.results);
+  console.log(markupUpcomingMovie);
+}
+catch (error) {
+  console.log(error);
+}
+}
+
--- src/js/upcoming.js
+++ src/js/upcoming.js
@@ -1,1 +1,29 @@
 
+const refs = {
+    galleryOfNewMovies: document.querySelector('.upcoming'),
+    remindBtn: document.querySelector('.upcoming__button')
+    
+  }
+  console.log(refs.remindBtn);
+
+refs.remindBtn.addEventListener('click', onAddMoreMovieBtn);
+
+function createUpcominfOfNewMovieCard(results) {
+    console.log(results);
+    return results.map(({original_title, release_date, vote_average, vote_count, popularity, genre_ids, overview}) => {
+     return `<div class="upcoming__info">
+     <img src="https://image.tmdb.org/t/p/original/${backdrop_path}" alt="${original_title}" loading="lazy" /></a>
+     <ul class="upcoming__list">
+     <li class ="upcoming__item"><span>${original_title}</span></li>
+     <li class ="upcoming__item"><span class="upcoming__item-font">Release date </span>${release_date}</li>
+     <li class ="upcoming__item"><span class="upcoming__item-font">Vote/Votes </span>${vote_average}/${vote_count}</li>
+     <li class ="upcoming__item"><span class="upcoming__item-font">Popularity </span>${popularity}</li>
+     <li class ="upcoming__item"><span class="upcoming__item-font">Genre </span>${genre_ids}</li>
+     <li class ="upcoming__item">
+     <span class="upcoming__item-uppercase">About
+      </span>${overview}</li>
+     </ul>
+     </div>`;
+   }).join('');
+  };
+  
