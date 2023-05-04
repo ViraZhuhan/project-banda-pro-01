a51123936824774441226ceaf8d951924245ba08
Artem-Podoliak <podoliak.a.s@gmail.com>
2023-05-04 16:16:44 +0300

    in the development process

--- src/js/weekly-trends/weeklyTrends.js
+++ src/js/weekly-trends/weeklyTrends.js
@@ -4,33 +4,35 @@ import GenresApi from './GenresApi';
 const weeklyTrendsApi = new WeeklyTrendsApi();
 const genresApi = new GenresApi();
 
-const ratings = document.querySelectorAll('.rating');
-if (ratings.length > 0) {
+const rating = document.querySelectorAll('.rating');
+
+if (rating.length > 0) {
   initRatings();
 }
 
 function initRatings() {
-  let ratingActive;
-  let ratingValue;
+  for (let index = 0; index < rating.length; index++) {
+    const ratingElement = rating[index];
+    initRating(ratingElement);
+  }
+}
 
-  for (let index = 0; index < ratings.length; index++) {
-    const rating = ratings[index];
-    initRating(rating);
-  }
-  function initRating(rating) {
-    initRatingVars(rating);
-    setRatingActiveWidth();
-  }
+function initRating(rating) {
+  const ratingActive = rating.querySelector('.rating__active');
+  const ratingValue = rating.querySelector('.rating__value');
+  const ratingItems = rating.querySelectorAll('.rating__item');
+  ratingItems.forEach(item => {
+    item.addEventListener('click', () => {
+      ratingValue.innerHTML = item.value;
+      setRatingActiveWidth(ratingActive, ratingValue);
+    });
+  });
+  setRatingActiveWidth(ratingActive, ratingValue);
+}
 
-  function initRatingVars(rating) {
-    ratingActive = rating.querySelector('.rating__active');
-    ratingValue = rating.querySelector('.rating__value');
-  }
-
-  function setRatingActiveWidth(index = ratingValue.innerHTML) {
-    const ratingActiveWidth = index / 0.5;
-    ratingActive.style.width = `${ratingActiveWidth}%`;
-  }
+function setRatingActiveWidth(ratingActive, ratingValue) {
+  const ratingActiveWidth = ratingValue.innerHTML / 0.025;
+  ratingActive.style.width = `${ratingActiveWidth}%`;
 }
 
 async function renderWeeklyTrends() {
@@ -38,7 +40,7 @@ async function renderWeeklyTrends() {
     const data = await weeklyTrendsApi.fetchMovie();
     const movies = data.results.slice(0, 3);
 
-    const wrapper = document.querySelector('.wrapper');
+    const wrapper = document.querySelector('.article__list');
     const genres = await genresApi.fetchGenres();
 
     const moviesElements = movies.map(movie => {
@@ -46,44 +48,48 @@ async function renderWeeklyTrends() {
         movie;
 
       const voteAverage = vote_average;
-
       const release = new Date(release_date).getFullYear();
       const movieGenres = genres
         .filter(genre => genre_ids.includes(genre.id))
         .map(genre => genre.name);
-
       const imageUrl = poster_path
         ? `https://image.tmdb.org/t/p/w500/${poster_path}`
         : 'https://via.placeholder.com/395x574?text=No+Image';
 
       return `
-        <li class="movie">
-          <img src="${imageUrl}" alt="${title}" width="395" haight="574">
+        <li class="article__item">
+        <article>
+        <img class="article__img" src="${imageUrl}" alt="${title}" width="395" >
           <div class="details">
-            <h3>${title}</h3>
-            <p>Release: ${release}</p>
+            <p class="title__txt">${title}</p>
+            <div class="wraper__details">
+            <p class="movieGenres">${movieGenres
+              .slice(0, 2)
+              .join(', ')} | ${release}</p>
             
-            <p>genre: ${movieGenres.join(', ')}</p>
             <div class="rating">
-            <div class="rating">
-            <div class="rating__body">
-              <div class="rating__active"></div>
-              <div class="rating__items">
-                <input type="radio" class="rating__item" value="1" name="rating" />
-                <input type="radio" class="rating__item" value="2" name="rating" />
-                <input type="radio" class="rating__item" value="3" name="rating" />
-                <input type="radio" class="rating__item" value="4" name="rating" />
-                <input type="radio" class="rating__item" value="5" name="rating" />
+              <div class="rating__body">
+                <div class="rating__active"></div>
+                <div class="rating__items">
+                  <input type="radio" class="rating__item" value="1" name="rating" />
+                  <input type="radio" class="rating__item" value="2" name="rating" />
+                  <input type="radio" class="rating__item" value="3" name="rating" />
+                  <input type="radio" class="rating__item" value="4" name="rating" />
+                  <input type="radio" class="rating__item" value="5" name="rating" />
+                </div>
               </div>
+              <div class="rating__value">${voteAverage}</div>
             </div>
-            <div class="rating__value">${voteAverage}</div>
+            </div>
           </div>
-          </div>
+        </article>
+          
         </li>
       `;
     });
 
     wrapper.innerHTML = moviesElements.join('');
+    initRatings();
   } catch (error) {
     console.error(error);
   }
