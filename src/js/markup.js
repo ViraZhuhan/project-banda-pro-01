5d8f76402f31255cf7ce77f63d924fdc0f114bf0
ViraZhuhan <virazhuhan@gmail.com>
2023-05-05 13:45:28 +0300

    add start files card-catalog

---
+++ src/js/markup.js
@@ -0,0 +1,29 @@
+import Api from './api';
+import cardsTpl from '../templates/cards.hbs';
+
+const gallery = document.querySelector('.gallery');
+
+const seachApi = new Api();
+
+async function createDayTrends() {
+  try {
+    const response = await seachApi.weekTrends(1);
+    console.log(response.results);
+    createGallery(response.results);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createGallery(films) {
+  clearGallery();
+  gallery.insertAdjacentHTML('beforeend', cardsTpl(films));
+}
+
+function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+createDayTrends();
+
+export { createDayTrends };
