13d781ae4024656b1f5d936329e49de2690e5406
ViraZhuhan <virazhuhan@gmail.com>
2023-05-06 13:45:13 +0300

    add render-cards

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -3,5 +3,6 @@ export default function getRefs () {
     return {
         rating: document.querySelectorAll('.rating'),
         wrapper: document.querySelector('.article__list'),
+        gallery: document.querySelector('.gallery')
       };
 }
