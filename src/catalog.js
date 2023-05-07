b4aff737f4660a41da62ebdeefcf4f7b91d60c38
cheptsova <116488910+cheptsova@users.noreply.github.com>
2023-05-07 12:01:35 +0300

    create scroll-up btn

--- src/catalog.js
+++ src/catalog.js
@@ -1,9 +1,8 @@
-
 import mobileMenu from './js/mobile-menu';
 import themeToggle from './js/themeToggle';
 import header from './js/header';
 import seachcatalog from './js/seachcatalog';
 // import { createDayTrends } from './js/markup';
 import modalMovie from './js/modal_movie';
-import {createWeekTrends} from './js/render-card'
-
+import { createWeekTrends } from './js/render-card';
+import scrollUp from './js/scroll-up';
