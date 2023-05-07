16243c61cf79192eb995867f626a51206baa901f
Vita Nychvoloda <vitanychvoloda@yahoo.com>
2023-05-07 15:01:41 +0300

    added localStorage Btn

--- src/js/upcoming.js
+++ src/js/upcoming.js
@@ -4,6 +4,8 @@ const refs = {
   remindBtn: document.querySelector('.upcoming__button')
   
 }
+
+
 const newMovies = new Api();
 
 fetchUpcomingMovie().then(renderUpcomingMovieCard)
@@ -35,8 +37,13 @@ const createUpcomingCard = results.map(result => {const {backdrop_path, original
     return release.getDate() + join + release.getMonth() + join + release.getFullYear();  
   }
   console.log(getFormatDate('.'));
-  
+
+  const formatDate = getFormatDate('.');
+  const title = res.results[0].original_title;
+  const voteAverage = res.results[0].vote_average;
+  const voteCount = res.results[0].vote_count;
   const idGenre = res.results[0].genre_ids;
+  const imageUrl = res.results[0].backdrop_path;
   // console.log(idGenre);
   let nameGenres=[];
   for(let i=0; i < idGenre.length; i+=1 ){
@@ -45,16 +52,17 @@ const createUpcomingCard = results.map(result => {const {backdrop_path, original
     nameGenres.push(item.name);
   }
   
+
   // console.log(nameGenres);
 return `<div class="upcoming__info"> 
-<img src="https://image.tmdb.org/t/p/original/${backdrop_path}" alt="${original_title}"  loading="lazy" class="upcoming__img" />
+<img src="https://image.tmdb.org/t/p/original/${imageUrl}" alt="${title}"  loading="lazy" class="upcoming__img" />
 <div class="upcoming__info-btn">
 <div class="upcoming__info-layout">
-<h3 class ="upcoming__movie-title"><span class="upcoming__item-font-title">${original_title}</span></h3>
+<h3 class ="upcoming__movie-title"><span class="upcoming__item-font-title">${title}</span></h3>
 <div class="upcoming__list-tablet">
 <ul class="upcoming__list">
-<li class ="upcoming__item"><span class="upcoming__item-font-date">Release date</span><span class="upcoming__item-data">${getFormatDate('.')}</span></li>
-<li class ="upcoming__item"><span class="upcoming__item-font-vote">Vote/Votes </span><span class="upcoming__item-vote">${vote_average}</span>  /  <span class="upcoming__item-votes">${vote_count}</span></li>
+<li class ="upcoming__item"><span class="upcoming__item-font-date">Release date</span><span class="upcoming__item-data">${formatDate}</span></li>
+<li class ="upcoming__item"><span class="upcoming__item-font-vote">Vote/Votes </span><span class="upcoming__item-vote">${voteAverage}</span>  /  <span class="upcoming__item-votes">${voteCount}</span></li>
 </ul>
 <ul class="upcoming__list">
 <li class ="upcoming__item"><span class="upcoming__item-font-popularity">Popularity </span><span class="upcoming__item-pop">${popularity}</span></li>
@@ -70,8 +78,27 @@ return `<div class="upcoming__info">
 });
 refs.galleryOfNewMovies.insertAdjacentHTML('beforeend', createUpcomingCard);
 
-} catch {error => console.log(error.message)};
+console.log(refs.remindBtn);
+console.log(refs.remindBtn.addEventListener('click', addLS));
+
+function addLS() {
+  const value = {
+    title: data.title,
+    popularity,
+    voteAverage,
+    img: imageUrl,
+    voteCount,
+    genres: nameGenres,
+    overview,
+  };
+  localStorage.setItem(key, JSON.stringify(value));
+  refs.remindBtn.disabled = true;
+  refs.remindBtn.style.backgraundColor = "grey";
 }
+
+} catch {error => console.log(error.message)};
+};
+
 
 
 // function createUpcomingCard(results) {
