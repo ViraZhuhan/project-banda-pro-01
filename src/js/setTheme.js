e31afd1c02a75d56fc4b77282ce63a9241a3d687
Olena Husliakova <113473257+softly242@users.noreply.github.com>
2023-05-07 18:04:24 +0200

    .

--- src/js/setTheme.js
+++
@@ -1,4 +0,0 @@
-// set color theme before page load to prevent flickering
-const html = document.documentElement;
-const savedTheme = localStorage.getItem('color-theme');
-if (savedTheme) html.className = savedTheme;
--- src/library.js
+++
@@ -1,6 +0,0 @@
-import mobileMenu from './js/mobile-menu';
-import themeToggle from './js/themeToggle';
-import header from './js/header';
-import scrollUp from './js/scroll-up';
-
-// LocalStorageService
