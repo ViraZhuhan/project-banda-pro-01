5fac50b4e42970cad6050bda267ccfdfe73128e5
ViraZhuhan <virazhuhan@gmail.com>
2023-05-08 14:11:09 +0300

    delete timeOut from modal-movie

--- src/js/api.js
+++ src/js/api.js
@@ -72,7 +72,6 @@ export default class Api {
 
     async getDetailsById(id) {
     const str = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
-    await new Promise(r => setTimeout(r, 1000));
     return fetch(str).then(res => res.json());
   }
 
