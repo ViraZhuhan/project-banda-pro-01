98e65c75bbc7f5ccc50354db29516a60a6771e00
TanyaBushik <tanya.bushik@gmail.com>
2023-05-08 16:08:18 +0300

    added changes

--- src/js/card-lbrary.js
+++ src/js/card-lbrary.js
@@ -1,30 +1,32 @@
-import { modalMovie } from './modal_movie';
+// import { modalMovie } from './modal_movie';
 import { markup } from './render-card';
+// import { refs } from './modal_movie';
 
 const noFilmsMessage = document.querySelector('.alert__message');
 
 window.addEventListener('load', onLoadPage);
 
-const key = 'data.title';
+// const KEY = 'LibraryMovie';
 // localStorage.setItem(key, JSON.stringify({ title: 123 }));
 // localStorage.removeItem(key);
 
 function onLoadPage() {
-  const data = localStorage.getItem(key);
-  // const moviesArr = JSON.parse(data);
+  const data = localStorage.getItem('LibraryMovie');
+  const moviesArr = JSON.parse(data);
 
-  if (data) {
-    markup(data);
+  if (localStorage.getItem('LibraryMovie')) {
+    console.log(localStorage.getItem('LibraryMovie'));
+    // markup();
   } else {
     noFilmsMessage.classList.remove('hidden');
   }
 }
 
-const movieClicked = document.querySelector('library-cards__list');
+// const movieClicked = document.querySelector('library-cards__list');
 
-movieClicked.addEventListener('click', onMovieClicked);
+// movieClicked.addEventListener('click', onMovieClicked);
 
-function onMovieClicked(event) {
-  let idOfCard = event.target.closest('.gallery__item ').id;
-  modalMovie(idOfCard);
-}
+// function onMovieClicked(event) {
+//   let idOfCard = event.target.closest('.gallery__item ').id;
+//   modalMovie(idOfCard);
+// }
