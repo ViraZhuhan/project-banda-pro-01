4eaacf75e78ef2308b95d6cba99de8220b44f3e4
VladimirPopovich27 <117470833+VladimirPopovich27@users.noreply.github.com>
2023-05-09 10:28:35 +0200

    update JS&SCSS

--- src/js/seachcatalog.js
+++ src/js/seachcatalog.js
@@ -1,99 +1,4 @@
-// import Api from './api';
-// import { Loading } from 'notiflix';
-// import { initRatings } from './init-rating';
-// import { noFilmError, onFetchError } from './msg-error';
-// import getRefs from './get-refs';
-// import { genresList } from './genre-list';
 
-// const searchApi = new Api();
-// const refs = getRefs();
-
-// const form = document.getElementById('search-form');
-// const input = document.getElementById('search-input');
-// const movieList = document.getElementById('movie-list');
-
-// form.addEventListener('submit', async (event) => {
-//   event.preventDefault();
-//   const searchQuery = input.value;
-//   movieList.innerHTML = '';
-//   try {
-//     Loading.pulse();
-//     const results = await searchApi.searchMovies(searchQuery);
-//     Loading.remove();
-//     if (results.length === 0) {
-//       const message = document.createElement('p');
-//       message.textContent = 'OOPS... We are very sorry! We don’t have any results due to your search.';
-//       movieList.appendChild(message);
-//     } else {
-//       results.forEach(result => {
-//         const movie = document.createElement('div');
-//         movie.classList.add('movie');
-//         movie.innerHTML = `
-//           <h2>${result.title}</h2>
-//           <img src="https://image.tmdb.org/t/p/w500${result.poster_path}" alt="${result.title}">
-//           <p>${result.overview}</p>
-//         `;
-//         movieList.appendChild(movie);
-//       });
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     Loading.remove();
-//     const message = document.createElement('p');
-//     message.textContent = 'Oops! Something went wrong.';
-//     movieList.appendChild(message);
-//   }
-// });
-
-// const form = document.getElementById('search-form');
-// const input = document.getElementById('search-input');
-// const movieList = document.getElementById('movie-list');
-// const MOVIE_URL = `https://api.themoviedb.org/3/`;
-// const API_KEY = '225e339996bc91260b33199c383c8881';
-
-// const searchForMovies = (apiKey, query) => {
-//   form.addEventListener('submit', (event) => {
-//     event.preventDefault();
-
-//     const searchQuery = input.value;
-
-//     // Clear the movie list
-//     movieList.innerHTML = '';
-
-//     // Call API to search for movies
-//     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`)
-//       .then(response => response.json())
-//       .then(data => {
-//         if (data.total_results === 0) {
-//           // No results found
-//           const message = document.createElement('p');
-//           message.textContent = 'OOPS... We are very sorry! We don’t have any results due to your search.';
-//           movieList.appendChild(message);
-//         } else {
-//           // Results found
-//           const results = data.results;
-//           results.forEach(result => {
-//             const movie = document.createElement('div');
-//             movie.classList.add('movie');
-//             movie.innerHTML = `
-//               <h2>${result.title}</h2>
-//               <img src="https://image.tmdb.org/t/p/w500${result.poster_path}" alt="${result.title}">
-//               <p>${result.overview}</p>
-//             `;
-//             movieList.appendChild(movie);
-//           });
-//         }
-//       })
-//       .catch(error => {
-//         console.error(error);
-//         const message = document.createElement('p');
-//         message.textContent = 'Oops! Something went wrong.';
-//         movieList.appendChild(message);
-//       });
-//   });
-// }
-// // Example usage:
-// searchForMovies('YOUR_API_KEY', '225e339996bc91260b33199c383c8881')
 const axios = require('axios').default;
 
 const API_KEY = '225e339996bc91260b33199c383c8881';
@@ -154,21 +59,34 @@ function getYear(dateString) {
   return dateString ? dateString.slice(0, 4) : '';
 }
 
+// async function renderMovies(movies) {
+//   let markup = '';
+//   for (const movie of movies) {
+//     if (!movie || !movie.poster_path) continue;
+//     markup += await renderMovie(movie);
+//   }
+//   movieList.innerHTML = markup;
+// }
 async function renderMovies(movies) {
   let markup = '';
-  for (const movie of movies) {
-    if (!movie || !movie.poster_path) continue;
-    markup += await renderMovie(movie);
+  if (movies.length === 0) {
+    markup = "<li class='cards__error'>OOPS...<br/>We are very sorry!<br/>We don’t have any results due to your search.</li > ";
+  } else {
+    for (const movie of movies) {
+      if (!movie || !movie.poster_path) continue;
+      markup += await renderMovie(movie);
+    }
   }
   movieList.innerHTML = markup;
 }
+
 
 async function handleSearchFormSubmit(e) {
   e.preventDefault();
   const query = searchInput.value;
   const movies = await searchMovies(query);
   await renderMovies(movies);
-  currentPage = 1;
+  // currentPage = 1;
 }
 
 function handlePrevButtonClick() {
@@ -191,7 +109,7 @@ nextButton.addEventListener('click', handleNextButtonClick);
 
 async function renderWeekMovies() {
   const movies = await searchMovies('week');
-  await renderMovies(movies);
+  // await renderMovies(movies);
 }
 
 renderWeekMovies();
